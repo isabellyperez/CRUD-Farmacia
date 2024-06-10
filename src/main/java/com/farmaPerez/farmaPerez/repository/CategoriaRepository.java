2977f508b74573654e2a09d241f1a0ef8af71776
isabellyperez <isabellyperez10@gmail.com>
2024-06-10 14:51:33 -0300

    Relacinamento entre classes criadas juntamente com todos os seus metodos. Projeto rodando no STS

--- src/main/java/com/farmaPerez/farmaPerez/repository/CategoriaRepository.java
+++ src/main/java/com/farmaPerez/farmaPerez/repository/CategoriaRepository.java
@@ -1,9 +1,15 @@
 package com.farmaPerez.farmaPerez.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.farmaPerez.farmaPerez.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
 
 }
--- src/main/java/com/farmaPerez/farmaPerez/repository/ProdutoRepository.java
+++ src/main/java/com/farmaPerez/farmaPerez/repository/ProdutoRepository.java
@@ -1,9 +1,14 @@
 package com.farmaPerez.farmaPerez.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.farmaPerez.farmaPerez.model.Produto;
 
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+	public List <Produto> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
